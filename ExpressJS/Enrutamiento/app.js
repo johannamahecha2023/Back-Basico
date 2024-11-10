f7b0decfbb6fd41f3898021feacd440280880089
Backend_Basic <johannamahechacp@gmail.com>
2024-11-09 20:34:32 -0500

    GET Y POST entre el cliente y el servidor

--- ExpressJS/Enrutamiento/app.js
+++ ExpressJS/Enrutamiento/app.js
@@ -2,19 +2,32 @@
 const express=require('express')
 
 const app=express()
+/*urlencoded permite obtener la informacion que 
+se envia en post por medio de la url o del cuerpo 
+de la solicitud*/
+
+app.use(express.urlencoded({extended: true}))
 
 //enrutamiento de nuestra pagina
-app.get('/home', (req,res) =>{
-    res.send(`estoy en la pagina de inicio`)
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname + '/index.html')
 })
 app.get('/iniciosesion', (req,res) =>{
-    res.send(`estas en inicio de sesion`)
+    res.sendFile(__dirname + '/iniciosesion.html')
 })
 app.listen(3000, ()=>{
    console.log(`se inicio por http://localhost:3000/`)
 })
-
+/*con el metodo post enviamos informacion pero tambien el usuario hace 
+una solicitud y adquiere una respuesta*/
+app.post('/Bienvenido',(req,res)=>{
+    res.send(`gracias por iniciar sesion`)
+    const usuario=req.body.usuario
+    const email=req.body.email
+    const password=req.body.password
+    res.send(`gracias por iniciar sesion`)
+})
 //para manejar rutas no definidas aplicamos el siguiente comodin
 app.get('*',(req,res)=>{
-    res.send(`pagina no encontrada`)
+    res.sendFile(__dirname + '/error.html')
 })
